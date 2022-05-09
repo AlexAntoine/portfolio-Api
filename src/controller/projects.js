80f7525091348c24d5623ce940741476b22d3f51
AlexAntoine <anotoinealex1234@gmail.com>
2022-05-09 00:45:14 -0400

    added asynchandler middleware

--- src/controller/projects.js
+++ src/controller/projects.js
@@ -1,5 +1,7 @@
 const projects = require('../models/project');
 const upload = require('../imageUpload');
+const asyncHandler = require('../middleware/aysnc');
+const ErrorResponse = require('../../utils/errorResponse');
 
 // use upload.single to upload images
 
@@ -16,9 +18,18 @@ exports.getAllProjects = async(req, res, next)=>{
     
 }
 
-exports.getSingleProject = async(req, res, next)=>{
-    res.status(200).json({sucess:true, msg:'Hello from get single project'});
-}
+exports.getSingleProject = asyncHandler(async(req, res, next)=>{
+
+    const project = await projects.findOne({name:req.params.name});
+    
+    if(!project)
+    {
+       return next(new ErrorResponse(`Bootcamp cannot be found with name of ${req.params.name}`, 404));
+    }
+
+    res.status(200).json({sucess: true, data:project});
+   
+});
 
 exports.createProject = async(req, res, next)=>{
 
